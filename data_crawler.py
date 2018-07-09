9478e18f00edb18ae4538158836f6ff5c8cb68bd
Suen Kai Ching <kc.suen@towngas.com>
2018-07-09 14:58:58 +0800

    Completed all crawling but catergort_list

--- data_crawler.py
+++ data_crawler.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup
-from chatterbot import ChatBot
 from selenium import webdriver
 import re
 import json
+import treelib
 from treelib import Node, Tree
+
 
 class DataCrawler():
     USERNAME = "KC_SUEN"
@@ -15,6 +16,8 @@ class DataCrawler():
     DIRECTORY = ["RootFolder"]
     URL_suffix = ".aspx"
 
+    EXCEPTION_MENU_ITEM = "Service Level Agreement"
+
     browser = None
     dataTree = None
 
@@ -22,7 +25,7 @@ class DataCrawler():
         self.browser = webdriver.Chrome()
         self.array = []
         self.dataTree = Tree()
-        self.dataTree.create_node("Homepage", "homepage", data=self.URL)
+        self.dataTree.create_node("Homepage", "homepage/", data=self.URL)
 
     def login(self):
         self.browser.get(self.LOGIN_URL)
@@ -37,6 +40,7 @@ class DataCrawler():
         return html
 
     def get_HTML_From_URL(self, url):
+        print("Accessing " + str(url))
         self.browser.get(url)
         html = self.browser.page_source
         return html
@@ -44,42 +48,78 @@ class DataCrawler():
     def get_menu(self, soup):
         result = soup.find(class_="menu vertical menu-vertical")
         result_in_static = result.findAll("li", class_="static")
-
         for ele in result_in_static:
-            print("-------------------------------------------------------")
+            # print("-------------------------------------------------------")
             name = ele.find(class_="menu-item-text")
             inner_ele = ele.findAll("li", class_="dynamic")
             link = None
+            parentID = "homepage/"
             if(inner_ele == []):
-                link = self.parseLink(ele)
-            else: # FOR DEBUGGING
-                print(name.get_text()) # FOR DEBUGGING
-            self.dataTree.create_node(name.get_text(), name.get_text().lower(), data=link, parent="homepage")
+                # print(name.get_text())  # FOR DEBUGGING
+                link_tag, link = self.parseLink(ele)
+            self.dataTree.create_node(name.get_text(), parentID + name.get_text().lower() + "/", data=link,
+                                      parent=parentID)
+            if (self.isDirectory(link)):
+                _html = self.get_HTML_From_URL(link)
+                soup = BeautifulSoup(_html, "lxml")
+                self.parseTable(soup, parentID + link_tag.get_text().lower() + "/")
+            # else: # FOR DEBUGGING
+            #     print(name.get_text()) # FOR DEBUGGING
+
+            if(name.get_text() == "Technology Update"):
+                print("")
+            # self.dataTree.show(idhidden=False)
             for small_ele in inner_ele:
-                link = self.parseLink(small_ele)
-                self.dataTree.create_node(small_ele.get_text(), small_ele.get_text().lower(), data=link, parent=name.get_text().lower())
-            print("-------------------------------------------------------")
+                parentID = "homepage/" + name.get_text().lower() + "/"
+                link_tag, link = self.parseLink(small_ele, _parent=parentID)
+                print("CHECKING IF "+ str(link) +" IS DIRECTORY...")
+                #self.dataTree.show()
+               # self.dataTree.create_node(small_ele.get_text(), small_ele.get_text().lower(), data=link, parent=name.get_text().lower())
+                if (self.isDirectory(link)):
+                    _html = self.get_HTML_From_URL(link)
+                    soup = BeautifulSoup(_html, "lxml")
+                    self.parseTable(soup, parentID + link_tag.get_text().lower() + "/")
+            # print("-------------------------------------------------------")
 
     def parseLink(self, soup_result, _parent=None):
-        link = soup_result.a
-        temp = None
-        if (link != None):
-            temp = link.get('href')
-            print(link.get_text())
-            print(temp)
-            if (temp[0] == '/'):
-                temp = self.URL + temp
+        print("praseLink")
+        link_tag = soup_result.a
+        link = None
+        if (link_tag != None):
+            link = link_tag.get('href')
+            # print(link_tag.get_text())
+            # print(link)
+            if (link[0] == '/'):
+                link = self.URL + link
             if(_parent != None):
-                self.dataTree.create_node(link.get_text(), link.get_text().lower(), data=link, parent=_parent)
-        if(self.isDirectory(temp)):
-            _html = self.get_HTML_From_URL(temp)
-            soup = BeautifulSoup(_html, "lxml")
-            self.parseTable(soup)
-        return temp
+                print("############")
+                print("Tag: " + str(link_tag))
+                print("Text: " + link_tag.get_text())
+                print("Link: " + str(link))
+                print("Parent: " + str(_parent))
+                print("############")
+                # self.dataTree.show(idhidden=False)
+                # if(link_tag.get_text() == self.EXCEPTION_MENU_ITEM):
+                #     if(self.dataTree.contains(self.EXCEPTION_MENU_ITEM.lower())):
+                #         print("dfgdfgdgdfgdfgd")
+                #         return link_tag, link
 
-    def parseTable(self, soup_result):
-        # print("-------------------------------------------------------")
-        # print("parseTable")
+                try:
+                    self.dataTree.create_node(str(link_tag.get_text()), _parent + str(link_tag.get_text().lower() + "/"), data=link,
+                                              parent=_parent)
+                except treelib.tree.DuplicatedNodeIdError:
+                    print("duplicated")
+                    return link_tag, link
+
+        # if(self.isDirectory(temp)):
+        #     _html = self.get_HTML_From_URL(temp)
+        #     soup = BeautifulSoup(_html, "lxml")
+        #     self.parseTable(soup, link.get_text().lower())
+        return link_tag, link
+
+    def parseTable(self, soup_result, _parent=None):
+        print("-------------------------------------------------------")
+        print("parseTable")
         try:
             table_list = soup_result.findAll("table")
             for table in table_list:
@@ -87,12 +127,16 @@ class DataCrawler():
                     table_body = table.find('tbody')
                     row_list = table_body.findAll('td', attrs={"class": "ms-vb-title"})
                     for x in range(0,  len(row_list)):
-                        self.parseLink(row_list[x])
+                        link_tag, link = self.parseLink(row_list[x], _parent)
+                        if (self.isDirectory(link)):
+                            _html = self.get_HTML_From_URL(link)
+                            soup = BeautifulSoup(_html, "lxml")
+                            self.parseTable(soup, _parent+link_tag.get_text().lower()+"/")
                     break
 
         except AttributeError as e:
             print(e)
-        # print("-------------------------------------------------------")
+        print("-------------------------------------------------------")
 
     def isDirectory(self, link):
         if(link == None):
@@ -105,9 +149,6 @@ class DataCrawler():
                 isDirectory = True
         return isDirectory
 
-    def getJSON_Tree(self):
-        return self.dataTree.to_json(with_data=True)
-
     def writeToJSONFile(self, path, fileName, data):
         filePathNameWExt = './' + path + '/' + fileName + '.json'
         with open(filePathNameWExt, 'w') as fp:
@@ -117,12 +158,11 @@ class DataCrawler():
         _html = self.login()
         soup = BeautifulSoup(_html, "lxml")
         self.get_menu(soup)
-        tree_in_json = self.getJSON_Tree()
-        print(tree_in_json)
-        parsed = json.loads(tree_in_json)
-        # print(json.dumps(parsed, indent=4, sort_keys=True))
-        self.writeToJSONFile('./', 'training', parsed)
         self.dataTree.show()
+        tree_in_dict = self.dataTree.to_json(with_data=True)
+        tree_in_json = json.dumps(tree_in_dict, indent=4, sort_keys=True, ensure_ascii=False)
+        self.writeToJSONFile('./', 'training', tree_in_json)
+        self.dataTree.save2file('tree_diagiam.json')
 
 if __name__ == "__main__":
     crawler = DataCrawler()
